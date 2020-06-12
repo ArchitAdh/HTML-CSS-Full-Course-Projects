9d301084b6bfab49b196bc91c12290d3e4520525
Archit Adhikari <architadhikary@gmail.com>
2020-06-12 16:16:41 +0545

    added smooth scrool and transcluscent navbar functionality

--- Investment Firm website/js/main.js
+++ Investment Firm website/js/main.js
@@ -1,3 +1,4 @@
+// Map
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -5,3 +6,28 @@ function initMap() {
     zoom: 16,
   });
 }
+
+// Sticky menu opacity (vanila js)
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 70, //-70 px from top of hash
+      },
+      800 // scroll time in ms
+    );
+  }
+});
